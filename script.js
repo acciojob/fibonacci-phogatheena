190cc67f7c832a9e52fcb426dcdbf33623e7b32c
phogatheena <109434462+phogatheena@users.noreply.github.com>
2023-08-12 23:26:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
-var a=0;
-var b=1;
-if(num==1) return a;
-	
-	if(num==2) return b ;
-	
- //to store the sum
-
- //iterate till the given num
- for(let i = 3; i <= num; i++){
-    //sum of last two numbers
-   let c = a + b; 
-   
-    //assign the last value to first     
-    a = b; 
-
-    //assign the sum to the last
-    b = c; 
- }
- 
- //if the num is 0 then return a else return b;
- return b;
-}
+function fibonacci(num)
+	{
+		if(num)==0
+		return 0;
+		else if(num==1||num==2)
+			return 1;
+		else
+			return fibonacci(num-1)+fibonacci(num-2);
+	}
 
 module.exports = fibonacci;
