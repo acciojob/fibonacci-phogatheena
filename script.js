87fc64075ed1dd3f08816a60b056643c5aaaded2
phogatheena <109434462+phogatheena@users.noreply.github.com>
2024-01-10 01:17:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-	function fibonacci(num) {
-        if (num===1||num==0) {
-        return 0;
-    } 
-		else if (num === 2) {
-        return 1;
-    } 
-		var num1=0;
-		var num2=1;
-		for(let i =3;i<=num;i++)
-			{
-				let result=num1+num2;
-				num1=num2;
-				num2=result;
-			}
-		return result;
-	}
+function fibonacci(num) {
+  if (num === 0 ||num === 1 ) {
+    return 0;
+  }
+   if (num === 2 ) {
+    return 1;
+  }
+
+  var prev = 0;
+  var current = 1;
+
+  for (var i = 3; i <= num; i++) {
+    var next = prev + current;
+    prev = current;
+    current = next;
+  }
+
+  return current;
+}
+
 module.exports = fibonacci;
