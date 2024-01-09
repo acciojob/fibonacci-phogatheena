a5c0daa7c4104655fac76b34c8348ef0bfe32160
phogatheena <109434462+phogatheena@users.noreply.github.com>
2024-01-10 01:07:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,16 @@
         return 0;
     } else if (num === 2) {
         return 1;
-    } else {
-        return fibonacci(num - 1) + fibonacci(num - 2);
+    } 
+		var num1=0;
+		var num2=1;
+		for(let i =3;i<=n;i++)
+			{
+				let result=num1+num2;
+				num1=num2;
+				num2=result;
+			}
+		return result;
     }
 
 	}
